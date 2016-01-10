639a1288a27be193031d00a758a5aa13939c27cb
Peter <merrygoat@users.noreply.github.com>
2016-01-10 03:55:01 +0000

    Completed 3D tracking. Outputs values one column per Z-stack in output files. Probably still need to calculate a weighted average based on the number of samples for each column.

--- confocal/3Dconfocaltrackroutine.r
+++ confocal/3Dconfocaltrackroutine.r
@@ -5,19 +5,22 @@
 threeDconfocaltrackroutine = function(remove_drift = TRUE){
   
   # source all scripts in the current directory
-  setwd("/Users/pc9836/Documents/git/2d-particle-tracking")
+  #setwd("/Users/pc9836/Documents/git/2d-particle-tracking")
+  setwd("C:\\Users\\Peter\\Documents\\Uni\\PhD\\2d-particle-tracking")
   filelist <- c("pre_tracking/feature.r", "characterisation/gr2d.r", "tracking/iantrack.r", "tracking/driftremoval.R", "characterisation/isf.r", "pre_tracking/lowpass.r", "characterisation/msd.r", "pre_tracking/pretrack.r", "characterisation/shift.r", "characterisation/overcirc.r")
   sapply(filelist,source,.GlobalEnv)
   library(EBImage)
   
   #File directory variables
-  varfilename <- "/Volumes/WIN_DATA/Confocal/STED/15-12-21/images/FITC 19"
+  #varfilename <- "/Volumes/WIN_DATA/Confocal/STED/15-12-21/images/FITC 19"
+  varfilename <- "E:\\Confocal\\STED\\15-12-21\\images\\FITC 19"
   #put slash on end of dirname
-  vardirname <- "/Volumes/WIN_DATA/Confocal/STED/15-12-21/"
+  #vardirname <- "/Volumes/WIN_DATA/Confocal/STED/15-12-21/"
+  vardirname <- "E:\\Confocal\\STED\\15-12-21\\"
   
   #Pretrack variables
-  varimages <- 30        #How many image to read from varfilename
-  varzdepth <- 18
+  varimages <- 30         #How many image to read from varfilename
+  varzdepth <- 18         #How many z planes there are
   vardiameter <- 11       #Particle diameter - used in particle identification
   varfilter <- 11         #Parameter for lowpass filter
   varbgavg <- 11          #Parameter for lowpass filter
@@ -30,7 +33,7 @@ threeDconfocaltrackroutine = function(remove_drift = TRUE){
   
   #Other variables that I can't think of a title for
   varparticlesize = 19    #Used as the wavevector for isf
-  vartimestep = 60    #Frame time in seconds. Used for all data output to correct time in frames to time in seconds.
+  vartimestep = 60        #Frame time in seconds. Used for all data output to correct time in frames to time in seconds.
   vargofrframes = 64      #How many frames of data to analyse for the g(r)
   
   
@@ -41,49 +44,52 @@ threeDconfocaltrackroutine = function(remove_drift = TRUE){
   varimgx <- dim(testimg)[1]          #Width of the image in pixels
   varimgy <- dim(testimg)[2]          #Height of the image in pixels
   
-  particlecount <- rep(0, varimages-1)
+  allparticlecount <- matrix(seq(from=1, to=varimages-1))
+  allmsq  <- seq(from=1, to=varimages-1)
+  allmsq <- cbind(matrix(allmsq), allmsq*vartimestep)
+  allsamples <- seq(from=1, to=varimages-1)
   allfsqt <- seq(from=1, to=varimages-1)
   allfsqt <- cbind(matrix(allfsqt), allfsqt*vartimestep)
-  
+    
   for(i in 1:varzdepth){
+    cat("Frame", i, " of ", varzdepth, "\n")
     pt <- pretrack(filename=varfilename,images=varimages,diameter=vardiameter,filter=varfilter,bgavg=varbgavg, masscut=varmasscut,minimum=varminimum,chan="grey",zstack=TRUE,znumber=i)
     ptfilt <- which(pt[,1] > varedgecutoff & pt[,1] < (varimgx-varedgecutoff) & pt[,2] > varedgecutoff & pt[,2] < (varimgy-varedgecutoff))
   
     
-  #Get particle count for each image
-  newparticlecount <- rep(0, varimages-1)
-  for(i in 0:varimages-1) {newparticlecount[i] <- sum(pt[ptfilt,6] == i)}
-  particlecount <- cbind(matrix(particlecount), matrix(newparticlecount))
+    #Get particle count for each image
+    particlecount <- rep(0, varimages-1)
+    for(i in 0:varimages-1) {particlecount[i] <- sum(pt[ptfilt,6] == i)}
+    allparticlecount <- cbind(allparticlecount, matrix(particlecount))
     
-  tr <- iantrack(pretrack=pt[ptfilt,],maxdisp=varmaxdisp,imgsize=c(varimgx,varimgy),goodenough=10)
-  if(remove_drift == TRUE) 
-    {trnodrift <- driftremoval(tr, display_graphs = TRUE)}
+    tr <- iantrack(pretrack=pt[ptfilt,],maxdisp=varmaxdisp,imgsize=c(varimgx,varimgy),goodenough=10)
+    if(remove_drift == TRUE) {
+      # write to new array - would paste over "tr" but had trouble with "tr" not taking new values
+      trnodrift <- driftremoval(tr)
+    }
   
-  #write(t(tr),file=paste(vardirname, "raw_coords.txt", sep=""),ncolumns=7,sep="\t")
-  #Don't need to filter tr as it was already filtered from pt
-  #Do msd and write it out
-  #if(remove_drift == TRUE) 
-  #  {msq <- msd(trnodrift)}
-  #else
-  #  {msq <- msd(tr)}
-  #msq <- cbind(msq[,1], msq[,1]*vartimestep, msq[,2:6], msq[,6]/varparticlesize, msq[,7]) # multiply the first column by vartimestep to give time in seconds, divide the 6th column to give msd in dimeters
-  #msq <- rbind(c("Frames", "Time", "Mean dx", "Mean dy", "Mean dx^2", "Mean dy^2", "Mean dr^2 (Pixels)", "Mean dr^2 (Diameters)", "Particle Count"), msq)
-  #write(t(msq),file=paste(vardirname, "msd.txt", sep=""),ncolumns=9,sep="\t")
-  
-  
-  #Do isf and write it out
-  if(remove_drift == TRUE) 
-    {fsqt <- isf(trnodrift,length=19)}
-  else
-    {fsqt <- isf(tr,length=19)}
-  allfsqt <- cbind(matrix(allfsqt), fsqt[,4]) # multiply the first column by vartimestep to give time in seconds
-  #fsqt <- rbind(c("Frames", "Time", "Real", "Imaginary", "Modulus", "Samples"), fsqt)
+    #write(t(tr),file=paste(vardirname, "raw_coords.txt", sep=""),ncolumns=7,sep="\t")
+    #Don't need to filter tr as it was already filtered from pt
+    #Do msd and write it out
+    if(remove_drift == TRUE) 
+      {msq <- msd(trnodrift)}
+    else
+      {msq <- msd(tr)}
+    allmsq <- cbind(allmsq, msq[,6]/varparticlesize)  # multiply the first column by vartimestep to give time in seconds, divide the 6th column to give msd in dimeters
+    allsamples <- cbind(allsamples, msq[,7])
+    
+    #Do isf and write it out
+    if(remove_drift == TRUE) 
+      {fsqt <- isf(trnodrift,length=19)}
+    else
+      {fsqt <- isf(tr,length=19)}
+    allfsqt <- cbind(allfsqt, fsqt[,4]) # multiply the first column by vartimestep to give time in seconds
+    #fsqt <- rbind(c("Frames", "Time", "Real", "Imaginary", "Modulus", "Samples"), fsqt)
   }
   
-  write(t(fsqt), file=paste(vardirname, "isf.txt", sep=""), ncolumns=6, sep="\t")
-  
-  particlecount <- cbind(seq(1,varimages-1), seq(1,varimages-1)*vartimestep, matrix(particlecount))
-  particlecount <- rbind(c("Frames", "Time", "Particle Count"), particlecount)
-  write(t(particlecount),file=paste(vardirname, "particlecount.txt", sep=""),ncolumns=varzdepth+2,sep="\t")
-  
+  write(t(allparticlecount), file=paste(vardirname, "particlecount.txt", sep=""), ncolumns=1+varzdepth, sep="\t")
+  write(t(allfsqt), file=paste(vardirname, "isf.txt", sep=""), ncolumns=2+varzdepth, sep="\t")
+  write(t(allmsq),file=paste(vardirname, "msd.txt", sep=""),ncolumns=2+varzdepth,sep="\t")
+  write(t(allsamples),file=paste(vardirname, "samples.txt", sep=""),ncolumns=varzdepth+1,sep="\t")
+
 }
