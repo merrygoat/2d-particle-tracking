6074e2fc08799098305124170821463770b39b28
Peter <merrygoat@users.noreply.github.com>
2016-07-05 16:28:06 +0100

    Added posttrack.r to allow for tracking of pre identified coordinates. Slight modifications to iantrack to cope with new format.

---
+++ characterisation/posttrack.R
@@ -0,0 +1,77 @@
+#!/usr/bin/env Rscript
+
+# Tracking and characterisation of pre-identified particles
+
+posttrack = function(remove_drift = FALSE){
+  
+  # source all scripts in the current directory
+  setwd("C:\\Users\\Peter\\Documents\\Uni\\PhD\\2d-particle-tracking")
+  filelist <- c("characterisation/gr2d.r", "tracking/iantrack.r", "tracking/driftremoval.R", "characterisation/isf.r", "characterisation/msd.r", "characterisation/shift.r")
+  sapply(filelist,source,.GlobalEnv)
+  library(EBImage)
+  
+  #File directory variables
+  varfilename <- "C:\\Users\\Peter\\Dropbox\\tracking\\averaged_coords.txt"
+  #put slash on end of dirname
+  vardirname <- "C:\\Users\\Peter\\Dropbox\\tracking\\"
+  
+  #Track variables
+  varedgecutoff <- 10     #Set to same value as used in particle identification
+  varmaxdisp <- 5         #Used in tracking - the maximum allowed interframe displacement
+  
+  #Other variables that I can't think of a title for
+  varparticlesize = 15    #Used as the wavevector for isf
+  vartimestep = 1     #Frame time in seconds. Used for all data output to correct time in frames to time in seconds.
+  vargofrframes = 10      #How many frames of data to analyse for the g(r)
+  
+  
+  ### Main ###
+  
+  # Particles come in three tab seperated columns: x, y, framenumber
+  pt <- read.delim(varfilename, header=FALSE, sep="\t")
+  # Bulk out pt with a few blank columns so that iantrack doesn't complain
+  pt <- cbind(pt[, 1:2], matrix(0,nrow = length(pt[, 1]), ncol = 3), pt[, 3])
+  
+  varimages <- max(pt[, 6])
+  varimgx <- max(pt[, 1])          #Width of the image in pixels
+  varimgy <- max(pt[, 2])          #Height of the image in pixels
+  
+  
+  #Get particle count for each image
+  particlecount <- rep(0, varimages-1)
+  for(i in 0:varimages-1) {particlecount[i] <- sum(pt[, 6] == i)}
+  particlecount <- cbind(seq(1,varimages-1), seq(1,varimages-1)*vartimestep, matrix(particlecount))
+  particlecount <- rbind(c("Frames", "Time", "Particle Count"), particlecount)
+  write(t(particlecount),file=paste(vardirname, "particlecount.txt", sep=""),ncolumns=3,sep="\t")
+  
+  tr <- iantrack(pretrack=pt[],maxdisp=varmaxdisp,imgsize=c(varimgx+varedgecutoff,varimgy+varedgecutoff),goodenough=3)
+  if(remove_drift == TRUE) 
+  {trnodrift <- driftremoval(tr, display_graphs = FALSE)}
+  
+  write(t(tr),file=paste(vardirname, "tracked_coords.txt", sep=""),ncolumns=7,sep="\t")
+  #Don't need to filter tr as it was already filtered from pt
+  #Do msd and write it out
+  if(remove_drift == TRUE) 
+  {msq <- msd(trnodrift)}
+  else
+  {msq <- msd(tr)}
+  msq <- cbind(msq[,1], msq[,1]*vartimestep, msq[,2:6], msq[,6]/varparticlesize, msq[,7]) # multiply the first column by vartimestep to give time in seconds, divide the 6th column to give msd in dimeters
+  msq <- rbind(c("Frames", "Time", "Mean dx", "Mean dy", "Mean dx^2", "Mean dy^2", "Mean dr^2 (Pixels)", "Mean dr^2 (Diameters)", "Particle Count"), msq)
+  write(t(msq),file=paste(vardirname, "msd.txt", sep=""),ncolumns=9,sep="\t")
+  
+  #Do isf and write it out
+  if(remove_drift == TRUE) 
+  {fsqt <- isf(trnodrift,length=varparticlesize)}
+  else
+  {fsqt <- isf(tr,length=varparticlesize)}
+  fsqt <- cbind(fsqt[,1], fsqt[,1]*vartimestep, fsqt[,2:5]) # multiply the first column by vartimestep to give time in seconds
+  fsqt <- rbind(c("Frames", "Time", "Real", "Imaginary", "Modulus", "Samples"), fsqt)
+  write(t(fsqt), file=paste(vardirname, "isf.txt", sep=""), ncolumns=6, sep="\t")
+  
+  #Do g(r) and write it out
+  gr <- gr2d(tr,nbins=200,deltar=0.5,imgsize=c(varedgecutoff,varedgecutoff,varimgx,varimgy), nframes=vargofrframes)
+  gr <- cbind(gr[,1], gr[,1]/varparticlesize, gr[,2]) # multiply the first column by vartimestep to give time in seconds
+  gr <- rbind(c("Distance (Diameters)", "Distance (Pixels)", "g(r)"), gr)
+  write(t(gr),file=paste(vardirname, "gr.txt", sep=""),ncolumns=3,sep="\t")
+  
+}
--- tracking/iantrack.r
+++ tracking/iantrack.r
@@ -13,15 +13,11 @@
 iantrack = function(pretrack,maxdisp,imgsize,goodenough=0,memory=FALSE){
   
   nnewsum <- 0
+  # Force standard column names to stop cbind getting angry
+  pretrack <- setNames(pretrack, c("V1",  "V2", "V3", "V4", "V5", "V6"))
   
   # We assume time starts at zero and increases to tmax
   tmax <- max(pretrack[,6])
-  
-  # Make output array of 6 columns to store:
-  # x, y, mass, r^2, frame, particle number
-  # 1 row for now, but we will ignore the first row at the end
-  output <- matrix(nrow=1,ncol=7)
-  #cat(output) #currently says NA NA NA NA NA NA
   
   # Label particles in first frame sequentially from 0
   # These form our initial particle identifications
@@ -30,11 +26,9 @@ iantrack = function(pretrack,maxdisp,imgsize,goodenough=0,memory=FALSE){
   zeroparticles <- nrow(zeropt)
   initident <- matrix(nrow=zeroparticles,ncol=1)
   initident[,1] <- seq(from=1,to=zeroparticles,by=1)
-  output <- rbind(output,cbind(zeropt,initident))
+  output <- cbind(zeropt,initident)
+  output <- setNames(output, c("V1",  "V2", "V3", "V4", "V5", "V6", "V7"))
   # So now particles in frame zero have an ID number 
-  
-  # Remove first row of output that contains a bunch of NA
-  output <- output[2:(zeroparticles+1),]
   
   # The ID for the next new particle is...
   nextnewparticle <- zeroparticles + 1
@@ -298,6 +292,7 @@ iantrack = function(pretrack,maxdisp,imgsize,goodenough=0,memory=FALSE){
     
     # Bind identifications to co-ords and then put this on the bottom of output
     nowout <- cbind(nowcoords,nowident)
+    nowout <- setNames(nowout, c("V1",  "V2", "V3", "V4", "V5", "V6", "V7"))
     output <- rbind(output,nowout)
       
   }
